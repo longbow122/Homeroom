94fbf4801e5957714fc1f47a899b2d7d3ba74f99
Longbow <longbow@dyescape.com>
2022-03-04 09:03:52 +0000

    Initial program commit.
    The first pieces of the program.

---
+++ src/main/java/me/longbow122/Homeroom/Main.java
@@ -0,0 +1,15 @@
+package me.longbow122.Homeroom;
+
+/**
+ * The main class which will store all called programs, and the main method to be run.
+ */
+public class Main {
+
+    /*
+    Main method to be called.
+    The called program
+     */
+    public static void main(String[] args) {
+
+    }
+}
